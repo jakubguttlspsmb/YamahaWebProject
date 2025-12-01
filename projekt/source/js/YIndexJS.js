7751493e522422a087bffe1743bcf0a656f53a11
jacobguttl <Jacobguttl2006@gmail.com>
2025-12-01 10:25:47 +0100

    img

--- projekt/source/js/YIndexJS.js
+++ projekt/source/js/YIndexJS.js
@@ -1,13 +1,10 @@
 function zmenObrazek(cesta, kliknutyElement) {
-  // 1. Najde hlavní obrázek a změní mu src
   document.getElementById("mainImage").src = cesta;
 
-  // 2. Všechny miniatury označí jako neaktivní
   var miniatury = document.querySelectorAll(".thumbnail");
   miniatury.forEach(function (miniatura) {
     miniatura.classList.remove("active");
   });
 
-  // 3. Kliknutou miniaturu označí jako aktivní
   kliknutyElement.classList.add("active");
 }
